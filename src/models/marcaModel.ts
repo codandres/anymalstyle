4f09354d24f916f749c7cd7272eeb7a049ce3dd4
Zuleima Muñoz <zulem090@gmail.com>
2024-04-06 22:21:17 -0500

    feat: marca y tipo models y dto

---
+++ src/models/marcaModel.ts
@@ -0,0 +1,13 @@
+'use server';
+
+import { MarcaDto } from "@/dto/marca/marcaDto";
+import prisma from "@/orm/prisma";
+import { Marca } from "@prisma/client";
+
+
+
+export async function getAllMarcas(): Promise<MarcaDto[]> {
+    const marcas: Marca[] = await prisma.marca.findMany();
+
+    return marcas.map((marca) => ({ idMarca: Number(tipo.idMarca), nombre: marca.nombre }));
+}
